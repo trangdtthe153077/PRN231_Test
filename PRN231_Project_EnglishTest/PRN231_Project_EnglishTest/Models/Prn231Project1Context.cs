1e45327f9818fabd1dd160ff62a09e533c05a9ec
duongth201 <hoduong201@gmail.com>
2023-07-24 22:46:07 +0700

    result page

--- PRN231_Project_EnglishTest/PRN231_Project_EnglishTest/Models/Prn231Project1Context.cs
+++ PRN231_Project_EnglishTest/PRN231_Project_EnglishTest/Models/Prn231Project1Context.cs
@@ -31,7 +31,7 @@ public partial class Prn231Project1Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local)\\SQLEXPRESS01;database=PRNPRJ4;Integrated security=true;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Server=(local);database=Prn231Project1;Integrated security=true;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
